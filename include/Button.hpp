8db91335f43a7e4522a16dc62408ecbf0ec80512
Gustav Ekner <gustav.ekner@gmail.com>
2015-08-03 18:39:30 +0200

    Create Button.hpp

---
+++ include/Button.hpp
@@ -0,0 +1,27 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <string>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include "ButtonBase.hpp"
+#include "functions.hpp"
+
+namespace gui
+{
+    class Button: public ButtonBase
+    {
+        friend class Gui;
+        public:
+            Button(std::string _label);
+            void addClickFunction(void (*_f)());
+            bool update();
+            void draw();
+        private:
+            void updateValues();
+            void (*clickFunction)();
+            bool clickCallback;
+    };
+}
+
+#endif // BUTTON_H
